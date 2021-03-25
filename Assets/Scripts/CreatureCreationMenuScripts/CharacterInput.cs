acd9cb04cd24b34260b2c65ce7f35087ceab8560
domx9200 <domx9200@gmail.com>
2021-03-25 01:05:56 -0400

    update to CharacterInput.cs
    removed the 20+ functions in favor of 1 function that finds the expected values. It also converts input to ints when needed.
    Also removed the scripts from the main camera, Michael thanked me for that.

--- Assets/Scripts/CreatureCreationMenuScripts/CharacterInput.cs
+++ Assets/Scripts/CreatureCreationMenuScripts/CharacterInput.cs
@@ -1,175 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CharacterInput : MonoBehaviour
-{
-        private string _creatureName;
-        private string _HP, _AC, _Initiative;
-        private string _strengthScore, _dexterityScore, _constitutionScore, _wisdomScore, _intelligenceScore, _charismaScore;
-        private string _strengthBonus, _dexterityBonus, _constitutionBonus, _wisdomBonus, _intelligenceBonus, _charismaBonus;
-        private string _strengthSave, _dexteritySave, _constitutionSave, _wisdomSave, _intelligenceSave,_charismaSave;
-        private string _passivePerception, _passiveInvestigation, _passiveInsight;
+public class CharacterInput : MonoBehaviour {
+    private string _creatureName;
+    private int _creatureHealth, _armorClass, _initiative;
+    //the array is going to be in the order of [0] = ability score, [1] = ability mod, [2] = ability save
+    //passives are in the order of [0] = perception, [1] = investigation, [2] = insight
+    private int[] _strength = new int[3], _dexterity = new int[3], _constitution = new int[3], 
+                  _intelligence = new int[3], _wisdom = new int[3], _charisma = new int[3], _passives = new int[3];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //I know that this function is a bit on the beefy side, but it's still better than keeping 20+ seperate functions.
+    public void inputValue(string input) {
+        int temp;
+        //start with seeing if it's the name because it doesn't need to be converted to an int
+        if(this.transform.parent.name == "creatureName") {
+            _creatureName = input;
+            Debug.Log(_creatureName);
+        //next three are prefab checks because they need special input
+        } else if(this.transform.name == "AbilityScoreInputField") {
+            if(int.TryParse(input, out temp)) {
+                findPrefab(this.transform.parent.name, 0, temp);
+            }
+        } else if(this.transform.name == "AbilityBonusInputField") {
+            if (int.TryParse(input, out temp)) {
+                findPrefab(this.transform.parent.name, 1, temp);
+            }
+        } else if (this.transform.name == "AbilitySaveInputField") {
+            if (int.TryParse(input, out temp)) {
+                findPrefab(this.transform.parent.name, 2, temp);
+            }
+        //the rest
+        } else {
+            if(int.TryParse(input, out temp)) {
+                //since we can't just automatically set the value based on name alone,
+                //this switch will be the selector
+                switch(this.transform.parent.name) {
+                    case "creatureHealth":
+                        _creatureHealth = temp;
+                        Debug.Log(_creatureHealth);
+                        break;
+                    case "armorClass":
+                        _armorClass = temp;
+                        Debug.Log(_armorClass);
+                        break;
+                    case "initiative":
+                        _initiative = temp;
+                        Debug.Log(_initiative);
+                        break;
+                    case "perception":
+                        _passives[0] = temp;
+                        Debug.Log(_passives[0]);
+                        break;
+                    case "investigation":
+                        _passives[1] = temp;
+                        Debug.Log(_passives[1]);
+                        break;
+                    case "insight":
+                        _passives[2] = temp;
+                        Debug.Log(_passives[2]);
+                        break;
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ReadName(string s)
-	{
-        _creatureName = s;
-        Debug.Log(_creatureName);
-	}
-
-    public void ReadHealth(string s)
-    {
-        _HP = s;
-        Debug.Log(_HP);
-    }
-
-    public void ReadArmor(string s)
-	{
-        _AC = s;
-        Debug.Log(_AC);
-	}
-
-    public void ReadInitiative(string s)
-	{
-        _Initiative = s;
-        Debug.Log(_Initiative);
-	}
-
-    public void ReadStrength(string s)
-	{
-        _strengthScore = s;
-        Debug.Log(_strengthScore);
-	}
-
-    public void ReadDexterity(string s)
-	{
-        _dexterityScore = s;
-        Debug.Log(_dexterityScore);
-	}
-
-    public void ReadConstitution(string s)
-	{
-        _constitutionScore = s;
-        Debug.Log(_constitutionScore);
-	}
-
-    public void ReadWisdom(string s)
-	{
-        _wisdomScore = s;
-        Debug.Log(_wisdomScore);
-	}
-
-    public void ReadIntelligence(string s)
-	{
-        _intelligenceScore = s;
-        Debug.Log(_intelligenceScore);
-	}
-
-    public void ReadCharisma(string s)
-	{
-        _charismaScore = s;
-        Debug.Log(_charismaScore);
-	}
-
-    public void ReadStrengthBonus(string s)
-    {
-        _strengthBonus = s;
-        Debug.Log(_strengthBonus);
-    }
-
-    public void ReadDexterityBonus(string s)
-    {
-        _dexterityBonus = s;
-        Debug.Log(_dexterityBonus);
-    }
-
-    public void ReadConstitutionBonus(string s)
-    {
-        _constitutionBonus = s;
-        Debug.Log(_constitutionBonus);
-    }
-
-    public void ReadWisdomBonus(string s)
-    {
-        _wisdomBonus = s;
-        Debug.Log(_wisdomBonus);
-    }
-
-    public void ReadIntelligenceBonus(string s)
-	{
-        _intelligenceBonus = s;
-        Debug.Log(_intelligenceBonus);
-	}
-
-    public void ReadCharismaBonus(string s)
-    {
-        _charismaBonus = s;
-        Debug.Log(_charismaBonus);
-    }
-
-    public void ReadStrengthSave(string s)
-    {
-        _strengthSave = s;
-        Debug.Log(_strengthSave);
-    }
-
-    public void ReadDexteritySave(string s)
-    {
-        _dexteritySave = s;
-        Debug.Log(_dexteritySave);
-    }
-
-    public void ReadConstitutionSave(string s)
-    {
-        _constitutionSave = s;
-        Debug.Log(_constitutionSave);
-    }
-
-    public void ReadWisdomSave(string s)
-    {
-        _wisdomSave = s;
-        Debug.Log(_wisdomSave);
-    }
-
-    public void ReadIntelligenceSave(string s)
-	{
-        _intelligenceSave = s;
-        Debug.Log(_intelligenceSave);
-	}
-
-    public void ReadCharismaSave(string s)
-    {
-        _charismaSave = s;
-        Debug.Log(_charismaSave);
-    }
-
-    public void ReadPassivePerception(string s)
-	{
-        _passivePerception = s;
-        Debug.Log(_passivePerception);
-	}
-
-    public void ReadPassiveInvestigation(string s)
-    {
-        _passiveInvestigation = s;
-        Debug.Log(_passiveInvestigation);
-    }
-
-    public void ReadPassiveInsight(string s)
-    {
-        _passiveInsight = s;
-        Debug.Log(_passiveInsight);
+    //finds which prefab is being used and sets the respective value.
+    private void findPrefab(string name, int index, int valueToSet) {
+        switch(name) {
+            case "strength":
+                _strength[index] = valueToSet;
+                Debug.Log(_strength[index]);
+                break;
+            case "dexterity":
+                _dexterity[index] = valueToSet;
+                Debug.Log(_dexterity[index]);
+                break;
+            case "constitution":
+                _constitution[index] = valueToSet;
+                Debug.Log(_constitution[index]);
+                break;
+            case "intelligence":
+                _intelligence[index] = valueToSet;
+                Debug.Log(_intelligence[index]);
+                break;
+            case "wisdom":
+                _wisdom[index] = valueToSet;
+                Debug.Log(_wisdom[index]);
+                break;
+            case "charisma":
+                _charisma[index] = valueToSet;
+                Debug.Log(_charisma[index]);
+                break;
+        }
     }
 }
