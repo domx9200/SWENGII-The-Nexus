dd977df76c33fb0df46c211f1f1b64e69c65186a
Evan Smith <evansmith731@gmail.com>
2021-04-23 12:51:23 -0400

    Revert "Merge branch 'main' into StandaloneFileBrowser"
    This reverts commit b2b7797b1949a85272a9d7c2498587fbc1171167, reversing
    changes made to daee51a497294c5daeadcd57974291863a2be578.

--- Assets/Scripts/CombatMenuSceneScripts/DropDownHandler.cs
+++ Assets/Scripts/CombatMenuSceneScripts/DropDownHandler.cs
@@ -46,46 +46,7 @@ public class DropDownHandler : MonoBehaviour
 
     IEnumerator WaitForOpenAnimation(float delay = 0)
     {
-<<<<<<< Updated upstream
-=======
-        // Prevent double clicking
-        this.gameObject.transform.GetComponent<Button>().enabled = false;
-        this.gameObject.transform.parent.GetChild(2).GetComponent<Button>().enabled = false;
-
-        // Change size of scrollview
-        RectTransform contentRT = this.transform.parent.parent.parent.gameObject.GetComponent<RectTransform>();
-        contentRT.sizeDelta = new Vector2(0, contentRT.rect.height + 166.3731f);
-
-        // get init list
-        GameObject initList = transform.parent.parent.gameObject;
-        int currIndex = transform.parent.GetSiblingIndex(); // current creature in list
-        Debug.Log("Creature expanding is: " + initList.transform.GetChild(currIndex));
-
-        // Loop through all creatures below current creature and animate
-        for (int i = currIndex + 1; i < initList.transform.childCount; i++)
-        {
-            Debug.Log("Current Creature moving is: " + initList.transform.GetChild(i).name);
-            Animator anim = initList.transform.GetChild(i).GetComponent<Animator>();
-            Debug.Log("tst 1/1");
-            if (anim != null)
-            {
-                Debug.Log("tset 2/1");
-                anim.SetBool("open", true);
-                Debug.Log("test 3/1");
-            }
-            else
-            {
-                Debug.Log("DropDownHandler::No animator on creature: " + initList.transform.GetChild(i).name);
-            }
-        }
-
-        Debug.Log("test 4/1");
-
-        // Anaimate
->>>>>>> Stashed changes
         yield return new WaitForSeconds(delay);
-
-        Debug.Log("test 5/1");
 
         // Set all children active
         foreach (Transform child in _dropDown.transform)
@@ -93,55 +54,10 @@ public class DropDownHandler : MonoBehaviour
             child.gameObject.SetActive(!child.gameObject.activeSelf);
         }
         this.gameObject.SetActive(false);
-<<<<<<< Updated upstream
-=======
-
-        Debug.Log("test 6/1");
-
-        // Reset buttons
-        this.gameObject.transform.GetComponent<Button>().enabled = true;
-        this.gameObject.transform.parent.GetChild(2).GetComponent<Button>().enabled = true;
-
-        Debug.Log("test 7/1");
-
->>>>>>> Stashed changes
     }
 
     IEnumerator WaitForCloseAnimation(float delay = 0)
     {
-<<<<<<< Updated upstream
-=======
-        // Prevent double clicking
-        this.gameObject.transform.GetComponent<Button>().enabled = false;
-        this.gameObject.transform.parent.GetChild(1).GetComponent<Button>().enabled = false;
-
-        // Get init list
-        GameObject initList = transform.parent.parent.gameObject;
-        int currIndex = transform.parent.GetSiblingIndex(); // index of current creature in init list
-        Debug.Log("Creature collapsing is: " + initList.transform.GetChild(currIndex));
-
-        // Loop through all creatures below current creature and animate
-        for (int i = currIndex + 1; i < initList.transform.childCount; i++)
-        {
-            Debug.Log("Current Creature moving is: " + initList.transform.GetChild(i).name);
-            Animator anim = initList.transform.GetChild(i).GetComponent<Animator>();
-            Debug.Log("tast 1/2");
-            if (anim != null)
-            {
-                Debug.Log("Test 2/2");
-                anim.SetBool("open", false);
-                Debug.Log("test 3/2");
-            }
-            else
-            {
-                Debug.Log("DropDownHandler::No animator on creature: " + initList.transform.GetChild(i).name);
-            }
-        }
-
-        Debug.Log("test 4/2");
-
-        // Animate
->>>>>>> Stashed changes
         yield return new WaitForSeconds(delay);
         _dropDown.SetActive(!_dropDown.activeSelf);
         this.gameObject.SetActive(false);
